3859d210491caa525a9c0714c303222b4b7e9b5d
Anthony Sendra <anthony.sendra@gmail.com>
2017-07-05 11:32:04 +0200

    init step 1

---
+++ src/components/List.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <li v-for="item in items" :class="{empty: item.stock < 1}">
+      <p>
+        <span @click="decrease(item)">-</span> {{ item.name }} : {{ item.stock }} <span @click="increase(item)">+</span>
+      </p>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  .empty {
+    color: red
+  }
+</style>
+
+<script>
+  export default {
+    name: 'List',
+    data () {
+      return {
+        items: [
+          {name: 'Tee-shirt', stock: 10},
+          {name: 'Polo', stock: 0}
+        ]
+      }
+    },
+    methods: {
+      decrease (item) {
+        item.stock = item.stock - 1
+      },
+      increase (item) {
+        item.stock = item.stock + 1
+      }
+    }
+  }
+</script>
